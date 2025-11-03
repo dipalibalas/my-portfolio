1c600ad1d12cda1d66fdd97d1068d2c0a74f13ee
dipalibalas <dipalimbalas@gmail.com>
2025-11-03 18:29:51 +0530

    Added certification and publication section

--- app/page.tsx
+++ app/page.tsx
@@ -207,7 +207,23 @@ export default function Home() {
           </li>
         </ul>
       </section>
-    
+     {/* Contact Section */}
+    <section className="bg-gray-900 rounded-xl shadow-lg p-6 w-full mb-6">
+  <h2 className="text-2xl font-semibold text-pink-300 mb-3">Certification & Publication</h2>
+  <div className="flex justify-between items-center text-gray-200">
+    <ul className="flex flex-col space-x-8">
+      <li className="flex items-center space-x-3 mb-2">
+        <a href="https://drive.google.com/file/d/1JB68ncFN1p9hvBRH9hvsKpap2r97D5Ra/view?usp=drive_link" className="hover:underline">
+          Namste React
+        </a>
+      </li>
+      <li className="flex items-center space-x-3">
+        Dipali M. Balas, Shardul J. Agravat Video Summarization using CNN and Clustering Algorithm Journal of Applied Science and Computations - 2018.
+      </li>
+      
+    </ul>
+  </div>
+</section>
     {/* Contact Section */}
     {/* <section className="bg-gray-900 rounded-xl shadow-lg p-6 w-full mb-6">
   <h2 className="text-2xl font-semibold text-pink-300 mb-3">Contact</h2>
